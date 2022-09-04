8be8ed1e7ffaad7774fcb5a2f39eceb921cc615e
ahamedazmie5 <it20132750@my.sliit.lk>
2022-09-04 11:41:48 +0530

    Port Change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () =>
   console.log(`Server successfully  started on : ${PORT}`)
