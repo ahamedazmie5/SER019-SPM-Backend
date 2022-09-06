ce3aa79fabe156da64b7bb8ba8bd5d6827467459
krishmipaboda <krishmipabo@gmail.com>
2022-09-06 09:45:08 +0530

    HotelPackages

---
+++ models/HotelPackagesSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const HotelPackagesSchema = new mongoose.Schema({
+	Hotel_Name: {
+		type: String,
+		required: true,
+	},
+	Single_Room_Price: {
+		type: Number,
+		required: true,
+	},
+	Double_Room_Price: {
+		type: Number,
+		required: true,
+		unique: true,
+	},
+	Luxury_Room_Price: {
+		type: Number,
+		required: true,
+	},
+	Hotel_Contact: {
+		type: Number,
+		required: true,
+	},
+	Location: {
+		type: String,
+		required: true,
+	},
+    },
+    {
+        timestamps:true,
+    },
+);
+
+module.exports = User = mongoose.model("hotelpackages", HotelPackagesSchema);
---
+++ routes/HotelPackagesRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { getHotelPackagesByID, getHotelPackages, createHotelPackages, UpdateHotelPackages } = require("../controllers/HotelPackagesController");
+
+router.post("/createHotelPackages",createHotelPackages);
+router.get("/getAllHotelPackages",getHotelPackages);
+router.get("/getHotelPackages/:id",getHotelPackagesByID);
+router.patch("/updateHotelPackages/:id",UpdateHotelPackages);
+
+module.exports = router;
