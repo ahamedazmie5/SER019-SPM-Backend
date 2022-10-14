7695ab3c0ec5ebb74e21e2ffa5854adf54e3948b
Tharushi1212 <it20000608@my.sliit.lk>
2022-10-14 11:05:40 +0530

    tharushi backend

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -6,8 +6,6 @@ const BlogModal = require('../models/blogModel');
 const createBlogs = async (req, res) => {
   console.log('blogs');
   const blogDta = req.body;
-  console.log(blogDta, 'aaaaaaaaaaaa');
-
   const newBlog = new BlogModal(blogDta);
   console.log('saved data', newBlog);
   try {
@@ -25,8 +23,6 @@ const getBlogsById = async (req, res) => {
 
   try {
     const blogs = await BlogModal.findById(id);
-    console.log(blogs, 'ssssssssssssssssss');
-
     res.status(200).json(blogs);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -46,4 +42,43 @@ const getAllBlogs = async (req, res) => {
   }
 };
 
-module.exports = { createBlogs, getBlogsById, getAllBlogs };
+const removeBlogById = async (req, res) => {
+  const { id } = req.params;
+  const deleteBlog = {
+    _id: id,
+  };
+  console.log('blog delete function', id);
+
+  let data = await BlogModal.findByIdAndRemove(deleteBlog);
+
+  res.json({ data: data, msg: 'delete success' });
+};
+
+const updateBlogs = async (req, res) => {
+  const { id } = req.params;
+  const { title, subDescription, description, img } = req.body;
+  console.log('update blog function');
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No accept with id: ${id}`);
+
+  const updatedTopic = {
+    title: title,
+    subDescription: subDescription,
+    description: description,
+    img: img,
+    _id: id,
+  };
+
+  await BlogModal.findByIdAndUpdate(id, updatedTopic, { new: true });
+
+  res.json(updatedTopic);
+};
+
+module.exports = {
+  createBlogs,
+  getBlogsById,
+  getAllBlogs,
+  removeBlogById,
+  updateBlogs,
+};
--- routes/blogsRouter.js
+++ routes/blogsRouter.js
@@ -4,10 +4,13 @@ const {
   createBlogs,
   getBlogsById,
   getAllBlogs,
+  removeBlogById,
+  updateBlogs,
 } = require('../controllers/blogsController');
 
 router.post('/createBlogs', createBlogs);
 router.get('/getBlogById/:id', getBlogsById);
 router.get('/getAllBlogs', getAllBlogs);
-
+router.delete('/removeBlogById/:id', removeBlogById);
+router.put('/updateBlogById/:id', updateBlogs);
 module.exports = router;
